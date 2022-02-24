c43e83cc37466c1e565b2d66bac92d71d0acdc59
jdj2261 <wlseowhd12@naver.com>
2022-02-24 12:56:29 +0900

    Modified tic_tac_toe.py

--- tutorials/tic_tac_toe.py
+++ tutorials/tic_tac_toe.py
@@ -58,7 +58,6 @@ class TTTBoard:
         board.cur_player, board.next_player = board.next_player, board.cur_player
         return board
 
-    # player_2
     def evaluate_game(self):
         for row in range(self.size):
             win_cnt = 0
@@ -142,54 +141,52 @@ class TTTBoard:
         mcts = MCTS()
                 
         while True:
-            # try:
-            user_input = input('>> ')
-            if user_input == 'q': break
-            if user_input == '': continue
+            try:
+                user_input = input('>> ')
+                if user_input == 'q': break
+                if user_input == '': continue
+                
+                row = int(re.split(r',|,\s+| ', user_input)[0])
+                col = int(re.split(r',|, | ', user_input)[1])
+
+                if self.position[row, col] != self.empty:
+                    print('Already put!!')
+                    continue
+
+                self = self.move(row, col)
+                print(self)
+                
+                result = self.evaluate_game()
+                if result == GAME_RESULT.WIN:
+                    print('player "%s" has won the game!\n' % self.winner)
+                    break
+                elif result == GAME_RESULT.LOSE:
+                    print('player "%s" has won the game!\n' % self.winner)
+                    break
+                else:
+                    if self.is_finished():
+                        print('Game is drawn!\n')
+                        break
+
+                best_node = mcts.search(self)
+                self = best_node.state
+                print(self)
+
+                result = self.evaluate_game()
+                if result == GAME_RESULT.WIN:
+                    print('player "%s" has won the game!\n' % self.winner)
+                    break
+                elif result == GAME_RESULT.LOSE:
+                    print('player "%s" has won the game!\n' % self.winner)
+                    break
+                else:
+                    if self.is_finished():
+                        print('Game is drawn!\n')
+                        break
             
-            row = int(re.split(r',|,\s+| ', user_input)[0])
-            col = int(re.split(r',|, | ', user_input)[1])
-
-            if self.position[row, col] != self.empty:
-                print('Already put!!')
-                continue
-
-            self = self.move(row, col)
-            print(self)
-
-            result = self.evaluate_game()
-            if result == GAME_RESULT.WIN:
-                print('player "%s" has won the game!\n' % self.winner)
-                break
-            elif result == GAME_RESULT.LOSE:
-                print('player "%s" has won the game!\n' % self.winner)
-                break
-            else:
-                if self.is_finished():
-                    print('Game is drawn!\n')
-                    break
-
-            best_node = mcts.search(self)
-            self = best_node.state
-
-            # print board
-            print(self)
-
-            result = self.evaluate_game()
-            if result == GAME_RESULT.WIN:
-                print('player "%s" has won the game!\n' % self.winner)
-                break
-            elif result == GAME_RESULT.LOSE:
-                print('player "%s" has won the game!\n' % self.winner)
-                break
-            else:
-                if self.is_finished():
-                    print('Game is drawn!\n')
-                    break
-        
-            # except Exception as e:
-            #     print('  Error:', e)
-            #     print('  Invalid input!!')
+            except Exception as e:
+                print('  Error:', e)
+                print('  Invalid input!!')
 
 
 if __name__ == "__main__":
